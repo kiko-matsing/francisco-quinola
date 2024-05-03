cd994dbfbe421c62f37952440b17328cf9407e72
Martin Jaycy Halum <martin.halum@legalmatch.com>
2024-05-03 16:12:03 +0800

    Add baseName

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="/francisco-quinola">
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
